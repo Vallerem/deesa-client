2d873799f9788f0d316cc3053be7d430dfcd98f8
Javier Armero <Javalba90@gmail.com>
2017-11-05 18:04:33 +0100

    design layout finished

--- src/app/components/designs-item/designs-item.component.ts
+++ src/app/components/designs-item/designs-item.component.ts
@@ -11,9 +11,43 @@ export class DesignsItemComponent implements OnInit {
    * params: {_id, title, creator, description, designGallery[], designMainImg, likedBy[]}
    */
   @Input() designsItem;
+  hover:string = 'opacity-no';
+  flagP = false;
+
 
   constructor() { }
 
   ngOnInit() {}
+
+
+
+  changeImgHover(event){
+    let str = event.target.innerHTML;
+    console.log("DIV");
+
+/*     console.log("mouse : " );
+    console.log("event type");
+    console.log(event.type); */
+    this.hover = (event.type === 'mouseenter') ? 'opacity-yes' : 'opacity-no';
+    console.log(this.hover);
+  }
+
+  changeImgHover2(event){
+    console.log("P");
+    this.hover='opacity-yes';
+    console.log(this.hover);
+
+ }
+
+
+
+/*   $(document).ready(function() {
+    $('#second, #third').hover(function(){
+        $('#first').addClass('blue');
+    },
+    function(){
+        $('#first').removeClass('blue');
+    });
+}); */
 
 }
