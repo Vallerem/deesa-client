1be0c90270f3dbac39e51debb571a6edafbd94ac
Javier Armero <Javalba90@gmail.com>
2017-11-05 21:22:59 +0100

    account fixes & home modified

--- src/app/components/account-info/account-info.component.ts
+++ src/app/components/account-info/account-info.component.ts
@@ -8,34 +8,20 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 export class AccountInfoComponent implements OnInit {
   optionalParameter: string;
   currentUser: any = JSON.parse(localStorage.getItem('user'));
- 
 
-  arr = [];
 
-  @Input() accountInfo: any = {}; // prevent an undefined element
+  @Input() accountInfo: any;
   @Input() currentView: any;
   @Output() submittedForm = new EventEmitter < boolean > ();
 
   constructor() {}
 
   ngOnInit() {
-         console.log("ngOnInit");
-      this.arr.push(this.accountInfo);
-
-        this.arr.forEach((element)=>{
-      console.log(element);
-        });
+    this.accountInfo.user.password="";
   }
 
   submitForm() {
-
-console.log("submitForm-->accountInfo");
-this.arr.push(this.accountInfo);
-    this.arr.forEach((element)=>{
-      console.log(element);
-        });
-    this.accountInfo.currentView = this.currentView; // add view: account/password/address
-
+     this.accountInfo.currentView = this.currentView; // add view: account/password/address
     this.submittedForm.emit(this.accountInfo); //Output - Send to parent
   }
 
--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -23,26 +23,18 @@ export class AccountComponent implements OnInit {
   accountInfo: any; //JSON
   userAvatar: any = JSON.parse(localStorage.getItem('user'));
   innerWidth = (window.screen.width) + "px";
-  arr = [];
   feedback;
+  message:any;
 
 
   constructor(private userAPI: UserService) {}
 
   ngOnInit() {
-    console.log("********************");
-    console.log(this.userAPI._currentUser);
-    
     this.userAPI.getAccount(this.userAPI._currentUser)
       .subscribe((res) => {
-        console.log(`ngOnInit[PARENT] subscribe response-->${JSON.stringify(res)}`);
         this.accountInfo = res;
         this.userAvatar = res.avatarUrl;
       });
-      this.arr.push(this.accountInfo);
-      this.arr.forEach((e)=>{
-        console.log(e);
-      })
 
     this.uploader.onSuccessItem = (item, response) => {
       this.feedback = JSON.parse(response).message;
@@ -56,51 +48,60 @@ export class AccountComponent implements OnInit {
   submitImage() {
 
     this.uploader.onBuildItemForm = (item, form) => {
-          item.withCredentials = false;
-          form.append('_id', this.userAPI._currentUser._id );
-          form.append('old_imgUrl', this.userAPI._currentUser.avatarUrl );       
-        };
+      item.withCredentials = false;
+      form.append('_id', this.userAPI._currentUser._id);
+      form.append('old_imgUrl', this.userAPI._currentUser.avatarUrl);
+    };
 
-        this.uploader.uploadAll();
+    this.uploader.uploadAll();
   }
 
   editAccount(accountInfo) {
 
-    console.log("account.component");
-
     if (accountInfo.currentView === 'Account') {
+      this.message="Cuenta mofificada";
       this.userAPI.editAccount(accountInfo.user)
-      .subscribe((res) => {
-        this.accountInfo = res;
-        console.log(`response editAccount: ${this.accountInfo}`);
+        .subscribe((res) => {
+          this.accountInfo.user.userInfo = res.user.userInfo;
+          console.log(`response editAccount:`);
+          console.log(this.accountInfo);
 
-      });
+        });
     }
 
     if (accountInfo.currentView === 'Password') {
+      //this.message="Contraseña modificada";
       this.userAPI.editPasswordAccount(accountInfo.user)
-      .subscribe((res) => {
-        this.accountInfo = res;
-        console.log(`response password: ${this.accountInfo}`);
+        .subscribe((res) => {
+          //this.accountInfo = res;
+          this.message = res.message;
+          console.log(`response password: ${this.accountInfo}`);
 
-      });
+        });
     }
 
     if (accountInfo.currentView === 'Address') {
+      this.message = "Direccion mofificada";
       this.userAPI.editAddressAccount(accountInfo.user)
-      .subscribe((res) => {
-        this.accountInfo = res;
-        console.log(`response editAccount: ${this.accountInfo}`);
-      });
+        .subscribe((res) => {
+          this.accountInfo.user.addressInfo = res.user.addressInfo;
+          console.log(`response editAccount: ${this.accountInfo}`);
+        });
     }
-
-
   }
 
   clicked(event) {
+    console.log("CLICK");
+    console.log("this.accountInfo");
+    console.log(this.accountInfo);
+    console.log("message:");
+
+    console.log(this.message);
+    this.message="";
+
+    //this.message = (function () { return; })(); // set message undefined
     event.preventDefault();
     this.currentView = event.target.innerHTML;
-    console.log(this.currentView);
-
   }
 }
+
