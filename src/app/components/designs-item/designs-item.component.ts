4d94a573286e7ee76856adffa3162d92f218cc88
Javier Armero <Javalba90@gmail.com>
2017-11-05 18:09:02 +0100

    clean code logs

--- src/app/components/designs-item/designs-item.component.ts
+++ src/app/components/designs-item/designs-item.component.ts
@@ -12,42 +12,16 @@ export class DesignsItemComponent implements OnInit {
    */
   @Input() designsItem;
   hover:string = 'opacity-no';
-  flagP = false;
-
 
   constructor() { }
 
   ngOnInit() {}
 
-
-
   changeImgHover(event){
-    let str = event.target.innerHTML;
-    console.log("DIV");
-
-/*     console.log("mouse : " );
-    console.log("event type");
-    console.log(event.type); */
     this.hover = (event.type === 'mouseenter') ? 'opacity-yes' : 'opacity-no';
-    console.log(this.hover);
   }
 
   changeImgHover2(event){
-    console.log("P");
     this.hover='opacity-yes';
-    console.log(this.hover);
-
  }
-
-
-
-/*   $(document).ready(function() {
-    $('#second, #third').hover(function(){
-        $('#first').addClass('blue');
-    },
-    function(){
-        $('#first').removeClass('blue');
-    });
-}); */
-
 }
