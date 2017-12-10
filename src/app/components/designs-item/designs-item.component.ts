ef8a81e036f77c328fbf97202f32ad2b430905c6
Javier Armero <Javalba90@gmail.com>
2017-12-10 16:26:29 +0100

    designs item design

--- src/app/components/designs-item/designs-item.component.ts
+++ src/app/components/designs-item/designs-item.component.ts
@@ -11,17 +11,17 @@ export class DesignsItemComponent implements OnInit {
    * params: {_id, title, creator, description, designGallery[], designMainImg, likedBy[]}
    */
   @Input() designsItem;
-  hover:string = 'opacity-no';
+/*   hover:string = 'opacity-no'; */
 
   constructor() { }
 
   ngOnInit() {}
 
-  changeImgHover(event){
+/*   changeImgHover(event){
     this.hover = (event.type === 'mouseenter') ? 'opacity-yes' : 'opacity-no';
   }
 
   changeImgHover2(event){
     this.hover='opacity-yes';
- }
+ } */
 }
